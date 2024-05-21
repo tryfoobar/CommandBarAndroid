5fd8f6399bef0b4edfdc4497d08faa3ed12effff
Michael Cavallaro <michaelcavallaro03@gmail.com>
2024-05-21 13:49:34 -0600

    chore: bump version to 1.0.7

--- commandbar/build.gradle.kts
+++ commandbar/build.gradle.kts
@@ -43,7 +43,7 @@ mavenPublishing {
 
         signAllPublications()
 
-        coordinates("com.commandbar.android", "commandbar", "1.0.6")
+        coordinates("com.commandbar.android", "commandbar", "1.0.7")
 
         pom {
             name.set("CommandBarAndroid")
